617f3813cfa95962e6cf6a01a6f7e7795650ac99
Naoki <murilomelo.code@gmail.com>
2023-07-03 11:41:45 -0300

    ad line .gitignore

--- src/crawlers/worthitorwoke.crawler.ts
+++ src/crawlers/worthitorwoke.crawler.ts
@@ -5,60 +5,62 @@ import { delay } from "../helpers";
 
 const proxy = new Proxy();
 export class worthItOrWoke implements Crawler {
-    showsNames: string[] = [];
-    showsAmount: number | null = null;
+  showsNames: string[] = [];
+  showsAmount: number | null = null;
 
-    async crawl() {
-        return await proxy.useProxy(async () => {
-            const { browser, page } = await puppeteerInstance()
+  async crawl() {
+    return await proxy.useProxy(async () => {
+      const { browser, page } = await puppeteerInstance();
 
-            await page.goto('https://worthitorwoke.com/category/recommend/');
+      await page.goto("https://worthitorwoke.com/category/recommend/");
 
-            const movieNames = new Set<string>();
+      const movieNames = new Set<string>();
 
-            while (true) {
-                const movieElements = await page.$$('h2.bt_bb_headline_tag a');
-                for (const element of movieElements) {
-                    const movieName = await page.evaluate((el) => el.textContent, element);
-                    movieNames.add(movieName as string);
-                }
-
-                console.clear()
-                console.log(`[crawler] @wiow: ${movieNames.size} movies`)
-
-                const olderPostsLink = await page.$('p.pageNext a');
-                if (!olderPostsLink) {
-                    break;
-                }
-
-                await Promise.all([
-                    page.waitForNavigation(),
-                    olderPostsLink.click(),
-                ]);
-                await delay(1000);
-            }
-
-            await browser.close();
-
-            this.showsAmount = movieNames.size;
-            this.showsNames = [...movieNames];
-
-            return movieNames
-        });
-    }
-    getShowsNames() {
-        if (this.showsNames.length === 0) {
-            console.log('No shows found. use .crawl().then(() => shows.getShowsNames()) to get them');
-            return [];
+      while (true) {
+        const movieElements = await page.$$("h2.bt_bb_headline_tag a");
+        for (const element of movieElements) {
+          const movieName = await page.evaluate(
+            (el) => el.textContent,
+            element
+          );
+          movieNames.add(movieName as string);
         }
-        return this.showsNames
-    }
-    getShowsAmount() {
-        if (this.showsAmount === null) {
-            console.log('No shows found. use .crawl().then(() => shows.getShowsAmount()) to get them');
-            return null;
-        }
-        return this.showsAmount
-    }
 
+        console.log(`[crawler] @wiow: ${movieNames.size} movies`);
+
+        const olderPostsLink = await page.$("p.pageNext a");
+        if (!olderPostsLink) {
+          break;
+        }
+
+        await Promise.all([page.waitForNavigation(), olderPostsLink.click()]);
+        await delay(1000);
+      }
+
+      await browser.close();
+
+      this.showsAmount = movieNames.size;
+      this.showsNames = [...movieNames];
+
+      return movieNames;
+    });
+  }
+  getShowsNames() {
+    if (this.showsNames.length === 0) {
+      console.log(
+        "No shows found. use .crawl().then(() => shows.getShowsNames()) to get them"
+      );
+      return [];
+    }
+    return this.showsNames;
+  }
+  getShowsAmount() {
+    if (this.showsAmount === null) {
+      console.log(
+        "No shows found. use .crawl().then(() => shows.getShowsAmount()) to get them"
+      );
+      return null;
+    }
+    return this.showsAmount;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { notWokeShows } from "./crawlers";
+import { worthItOrWoke } from "./crawlers/worthitorwoke.crawler";
 import { saveSetAsJson } from "./helpers";
 import { startCronJobs } from "./schedules";
 
 const nws = new notWokeShows();
+const wiow = new worthItOrWoke();
 
-nws.crawl().then((moviesSet) => {
-  saveSetAsJson(moviesSet as Set<string>, "movies_nws");
+// nws.crawl().then((moviesSet) => {
+//   saveSetAsJson(moviesSet as Set<string>, "movies_nws");
+// });
+
+wiow.crawl().then((moviesSet) => {
+  saveSetAsJson(moviesSet as Set<string>, "movies_wiow");
 });
 //startCronJobs();
