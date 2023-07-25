edd0296c1d2e801e8e1ed884c16e5ef18b20b0d8
muriloux <murilomelo.code@gmail.com>
2023-07-25 16:48:57 -0300

    fix: add /movies to endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { NotWokeShows } from "./crawlers/notwokeshows.crawler";
 import { WorthItOrWoke } from "./crawlers/worthitorwoke.crawler";
 import { saveSetAsJson } from "./shared/helpers";
 import "dotenv/config";
+import { ApiService } from "./shared/services/api";
 
 export const nws = new NotWokeShows();
 export const wiow = new WorthItOrWoke();
+
+const apiService = new ApiService();
 
 async function crawlAll() {
   const nwsMovies = await nws.crawl();
@@ -15,7 +18,9 @@ async function crawlAll() {
 }
 
 async function main() {
-  crawlAll();
+  //crawlAll();
+  const movies = await apiService.getMovies();
+  console.log(movies);
   console.log("[crawlers] done");
 }
 
